447600bcc322ffce4d0dfaab6a9afa6af33d819c
Jonathan von Rad <114144046+jonny-vr@users.noreply.github.com>
2024-02-08 20:41:31 +0000

    feat: Added Functionality to use -B and -A flag in script

--- searchFile.sh
+++ searchFile.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 
-SEARCH="$1" # Suchwort
+# Variablen für die Optionen -B und -A initialisieren
+BEFORE=0
+AFTER=0
+SEARCH=""
+PDF_FILES=()
 
-for (( I=2 ; I <= $# ; I++ )) # für alle übergebenen Dateien prüfe nache Suchwort
+# Funktion, um die Optionen -B und -A zu verarbeiten
+process_options() {
+    while getopts ":B:A:" opt; do
+        case $opt in
+            B) BEFORE=$OPTARG;;
+            A) AFTER=$OPTARG;;
+            \?) echo "Ungültige Option: -$OPTARG" >&2;;
+        esac
+    done
+}
+
+# Hauptprogramm
+process_options "$@" # Alle übergebenen Optionen verarbeiten
+
+# Shift, um die verarbeiteten Optionen aus den Argumenten zu entfernen
+shift $((OPTIND - 1))
+
+# Das Suchwort aus den Argumenten extrahieren
+SEARCH="$1"
+shift
+
+# Die verbleibenden Argumente sind Dateien, die durchsucht werden sollen
+PDF_FILES=("$@")
+
+# Die Schleife durchläuft jede PDF-Datei und führt die Suche durch
+for FILE in "${PDF_FILES[@]}"
 do
-    pdftotext "${!I}" temp
-    if grep -qi "$SEARCH" temp ; then 
-        FILENAME="${!I}"
-        LENGTH=$((${#FILENAME} + 1)) # Länge des Dateinamens plus eins für das zusätzliche "-"
+    pdftotext "$FILE" temp
+    if grep -iq "$SEARCH" temp ; then
+        FILENAME="$FILE"
+        LENGTH=$((${#FILENAME} + 1))
         LINE=$(printf "%-${LENGTH}s" "-")
         echo "${LINE// /-}"
-        echo "${!I}" # Datei, in der Suchwort gefunden wurde
+        echo "$FILENAME"
         echo "${LINE// /-}"
-        grep -i "$SEARCH" temp # Alle Vorkommen der Datei
+        grep -i -B "$BEFORE" -A "$AFTER" "$SEARCH" temp
         echo ""
     fi
     rm temp
